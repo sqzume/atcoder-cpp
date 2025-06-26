b8eac5d2f7e7e94b3f5ac26b938fb9ca788927ea
sqzume <173809390+sqzume@users.noreply.github.com>
2025-06-26 23:12:44 +0900

    abc408 a

--- abc/abc408/a/main.cpp
+++ abc/abc408/a/main.cpp
@@ -1,4 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {}
+int main() {
+    int n, s;
+    cin >> n >> s;
+    bool ans = true;
+    vector<int> t(n + 1);
+    t[0] = 0;
+    for (int i = 1; i < n + 1; i++) {
+        cin >> t[i];
+        if (s < t[i] - t[i - 1]) {
+            ans = false;
+        }
+    }
+
+    if (ans) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
