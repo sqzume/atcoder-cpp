64ddeb0968d23db9a726efe7623e197697497bbc
sqzume <173809390+sqzume@users.noreply.github.com>
2025-07-10 21:17:02 +0900

    Add: adt 2025-07-08

---
+++ adt/adt_easy_20250708_3/a/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ===== エイリアス =====
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+
+#define rep(i, x, y) for (int i = (x); i < (y); i++)
+#define CIN(type, var) type var; cin >> var
+#define CIN2(type, var1, var2) type var1, var2; cin >> var1 >> var2
+#define CIN3(type, var1, var2, var3) type var1, var2, var3; cin >> var1 >> var2 >> var3
+#define VECCIN(x) for (auto&youso_ : (x) ) cin >> youso_
+#define COUT(x) cout << (x) << '\n'
+#define SCOUT(x) cout << (x) << " "
+#define NCOUT(x) cout << (x)
+#define Yes cout << "Yes" << '\n'
+#define No cout << "No" << '\n'
+
+// ===== main() =====
+int main() {
+    CIN(int, n);
+    CIN2(string, s, t);
+
+    int ans = 0;
+    rep(i, 0, n) {
+        if (s[i] != t[i]) {
+            ans++;
+        }
+    }
+    COUT(ans);
+    return 0;
+}
---
+++ adt/adt_easy_20250708_3/e/main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ===== エイリアス =====
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+
+#define rep(i, x, y) for (int i = (x); i < (y); i++)
+#define CIN(type, var) type var; cin >> var
+#define CIN2(type, var1, var2) type var1, var2; cin >> var1 >> var2
+#define CIN3(type, var1, var2, var3) type var1, var2, var3; cin >> var1 >> var2 >> var3
+#define VECCIN(x) for (auto&youso_ : (x) ) cin >> youso_
+#define COUT(x) cout << (x) << '\n'
+#define SCOUT(x) cout << (x) << " "
+#define NCOUT(x) cout << (x)
+#define Yes cout << "Yes" << '\n'
+#define No cout << "No" << '\n'
+
+// ===== main() =====
+int main() {
+    set<tuple<ll, ll, ll>> ans;
+    for (ll i = 1; i < 100000000000; i++) {
+        for (ll j = 1; j < 100000000000; j++) {
+            for (ll k = 1; k < 100000000000; k++) {
+                
+            }
+        }
+    }
+
+    return 0;
+}
