894deeb2a7a765305de7fb8e414a1b52bc46bcbb
sqzume <173809390+sqzume@users.noreply.github.com>
2025-07-17 15:49:20 +0900

    Replace main.cpp

--- abc/abc404/c/main.cpp
+++ abc/abc404/c/main.cpp
@@ -1,4 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {}
+// ===== エイリアス =====
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define cout(x) cout << (x) << '\n'
+#define yes cout << "Yes" << '\n'
+#define no cout << "No" << '\n'
+
+int main() {
+
+    return 0;
+}
