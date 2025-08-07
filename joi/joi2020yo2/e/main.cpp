4b609e32a13f1c0a0dfedcdeba78a614ed364833
sqzume <173809390+sqzume@users.noreply.github.com>
2025-08-07 11:03:23 +0900

    Add joi-2020-2次予選

---
+++ joi/joi2020yo2/e/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ===== エイリアス =====
+using ll = long long;
+ll INF = 2e18;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define cout(x) cout << (x) << '\n'
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yes cout << "Yes" << '\n'
+#define no cout << "No" << '\n'
+
+// ===== デバッグ用 =====
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG
+#endif
+
+int main() {
+
+    return 0;
+}
