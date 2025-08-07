9ee954494252d46595c2bd143700238d5d20d814
sqzume <173809390+sqzume@users.noreply.github.com>
2025-08-07 11:28:14 +0900

    [JOI-2020-2次予選] Poster

--- joi/joi2020yo2/a/main.cpp
+++ joi/joi2020yo2/a/main.cpp
@@ -16,7 +16,51 @@ ll INF = 2e18;
 #define _GLIBCXX_DEBUG
 #endif
 
+vector<vector<char>> turn_right(vector<vector<char>> s, ll n) {
+    vector<vector<char>> u(n, vector<char>(n));
+    rep(i, n) {
+        rep(j, n) {
+            u[i][j] = s[n - 1 - j][i];
+        }
+    }
+    return u;
+}
+
+ll check(vector<vector<char>> s,vector<vector<char>> t, ll n) {
+    ll x = 0;
+    rep(i, n) {
+        rep(j, n) {
+            if (s[i][j] != t[i][j]) x++;
+        }
+    }
+    return x;
+}
+
 int main() {
+    ll n;
+    cin >> n;
+    vector<vector<char>> s(n, vector<char>(n)), t(n, vector<char>(n));
+    rep(i, n) {
+        rep(j, n) {
+            cin >> s[i][j];
+        }
+    }
+    rep(i, n) {
+        rep(j, n) {
+            cin >> t[i][j];
+        }
+    }
+
+    ll x = INF, y = 0;
+    chmin(x, check(s, t, n) + y);
+    y = 1; s = turn_right(s, n);
+    chmin(x, check(s, t, n) + y);
+    y = 2; s = turn_right(s, n);
+    chmin(x, check(s, t, n) + y);
+    y = 1; s = turn_right(s, n);
+    chmin(x, check(s, t, n) + y);
+
+    cout(x);
 
     return 0;
 }
