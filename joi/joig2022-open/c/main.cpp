959661c13c10e9f4a00a27d134c5b33c44928333
sqzume <173809390+sqzume@users.noreply.github.com>
2025-09-11 23:03:27 +0900

    [AC] joig2022_本選_C

--- joi/joig2022-open/c/main.cpp
+++ joi/joig2022-open/c/main.cpp
@@ -1,4 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (n); i++)
 
-int main() {}
+int main() {
+    // 入力
+    ll n;
+    cin >> n;
+
+    vector<ll> ag(n + 1, 0);
+    for (ll i = 1; i <= n; i++) {
+        ll x, y;
+        cin >> x >> y;
+        if (y <= ag[i - 1] - ag[i - 1 - x]) {
+            ag[i] = ag[i - 1] + 1;
+        } else {
+            ag[i] = ag[i - 1];
+        }
+    }
+    cout << ag[n] << endl;
+    return 0;
+}
