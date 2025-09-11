72ca668fddc7f9f38969489573ebe7e9eb869257
sqzume <173809390+sqzume@users.noreply.github.com>
2025-09-11 21:32:25 +0900

    [AC] dp_B

--- dp/b/main.cpp
+++ dp/b/main.cpp
@@ -1,14 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-// ===== エイリアス =====
 using ll = long long;
-#define rep(i, n) for (int i = 0; i < (n); i++)
-#define cout(x) cout << (x) << '\n'
-#define yes cout << "Yes" << '\n'
-#define no cout << "No" << '\n'
+#define rep(i, x, n) for (ll i = x; i < (n); i++)
 
 int main() {
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> h(n);
+    rep(i, 0, n) cin >> h[i];
 
+    vector<ll> dp(n, 1e18);
+    dp[0] = 0;
+    dp[1] = abs(h[1] - h[0]);
+    rep(i, 2, n) {
+        rep(j, 1, k + 1) {
+            if (i - j < 0) break;
+            dp[i] = min(dp[i - j] + abs(h[i] - h[i - j]), dp[i]);
+        }
+    }
+    cout << dp[n - 1] << endl;
     return 0;
 }
