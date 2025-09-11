2c054fd2a7bf46868e85269383320a002fcf6d7b
sqzume <173809390+sqzume@users.noreply.github.com>
2025-09-11 21:31:13 +0900

    [AC] joi2020_二次予選_C

--- joi/joi2020yo2/c/main.cpp
+++ joi/joi2020yo2/c/main.cpp
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-// ===== エイリアス =====
 using ll = long long;
-ll INF = 2e18;
-#define rep(i, n) for (int i = 0; i < (n); i++)
-#define cout(x) cout << (x) << '\n'
-#define chmax(x, y) x = max(x, y)
-#define chmin(x, y) x = min(x, y)
-#define yes cout << "Yes" << '\n'
-#define no cout << "No" << '\n'
-
-// ===== デバッグ用 =====
-#ifndef ONLINE_JUDGE
-#define _GLIBCXX_DEBUG
-#endif
+#define rep(i, n) for (ll i = 0; i < (n); i++)
 
 int main() {
+    ll n;
+    cin >> n;
 
+    vector<bool> dp(1e6 + 45, false);
+    dp[n] = true;
+    for (ll i = n - 1; 0 < i; i--) {
+        string s = to_string(i);
+        ll t = 0;
+        rep(i, s.size()) t += s[i]-'0';
+        if (dp[i + t]) dp[i] = true;
+    }
+    ll ans = 0;
+    rep(i, dp.size()) if (dp[i]) ans++;
+    cout << ans << endl;
     return 0;
 }
