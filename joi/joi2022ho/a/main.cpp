195ac0bf712b871e45b8f3112ed03286fdc24711
sqzume <173809390+sqzume@users.noreply.github.com>
2025-09-12 23:40:30 +0900

    [AC] joi2022_本選_B: インターカステラー

--- joi/joi2022ho/a/main.cpp
+++ joi/joi2022ho/a/main.cpp
@@ -4,5 +4,29 @@ using ll = long long;
 #define rep(i, n) for (ll i = 0; i < (n); i++)
 
 int main() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n), b(n, 1);
+    rep(i, n) cin >> a[i];
+    rep(i, n) {
+        while (a[i] % 2 == 0) {
+            a[i] /= 2;
+            b[i] *= 2;
+        }
+    }
+    vector<ll> c(n);
+    c[0] = b[0];
+    for (ll i = 1; i < n; i++) {
+        c[i] = c[i - 1] + b[i];
+    }
+
+    ll q;
+    cin >> q;
+    rep(i, q) {
+        ll x;
+        cin >> x;
+        auto s = lower_bound(c.begin(), c.end(), x);
+        cout << a[s - c.begin()] << endl;
+    }
     return 0;
 }
