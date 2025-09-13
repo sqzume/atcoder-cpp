c8714872efe1130bcba7fbadc53915ff71b6edb7
sqzume <173809390+sqzume@users.noreply.github.com>
2025-09-13 22:18:59 +0900

    [AC] joi2025_一次予選(第2回)_B: 鉄道旅行 3

--- joi/joi2025yo1b/b/main.cpp
+++ joi/joi2025yo1b/b/main.cpp
@@ -1,4 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (n); i++)
 
-int main() {}
+int main() {
+    ll p, q, a, b;
+    cin >> p >> q >> a >> b;
+    if (q <= p) {
+        cout << a * q << endl;
+    } else {
+        cout << a * p + (q - p) * b << endl;
+    }
+    return 0;
+}
