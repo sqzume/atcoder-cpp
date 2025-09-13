2a38d0c3b56c31399974ba8189074926577e697b
sqzume <173809390+sqzume@users.noreply.github.com>
2025-09-13 22:08:15 +0900

    [AC] joi2023_二次予選_B: ジョイ四人組

--- joi/joi2023yo2/b/main.cpp
+++ joi/joi2023yo2/b/main.cpp
@@ -1,4 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (n); i++)
 
-int main() {}
+int main() {
+    ll n;
+    cin >> n;
+    vector<vector<ll>> a(4, vector<ll>(n));
+    rep(i, 4) {
+        rep(j, n) {
+            cin >> a[i][j];
+        }
+    }
+    rep(i, 4) sort(a[i].begin(), a[i].end());
+
+    ll ans = LLONG_MAX;
+    rep(i, 4) {
+        rep(j, n) {
+            ll m = 0;
+            bool e = false;
+            rep(k, 4) {
+                if (k == i) continue;
+                auto it = lower_bound(a[k].begin(), a[k].end(), a[i][j]);
+                if (it == a[k].end()) {
+                    e = true;
+                    break;
+                }
+                ll w = a[k][it - a[k].begin()];
+                m = max(m, abs(a[i][j] - w));
+            }
+            if (e) continue;
+            ans = min(ans, m);
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
