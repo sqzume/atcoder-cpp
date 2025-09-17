8779b3dddb8546ac2c65b625d0c4675370eea477
sqzume <173809390+sqzume@users.noreply.github.com>
2025-09-17 16:26:27 +0900

    [AC] typical90-67: Base 8 to 9

--- typical90/067/main.cpp
+++ typical90/067/main.cpp
@@ -3,6 +3,37 @@ using namespace std;
 using ll = long long;
 #define rep(i, n) for (ll i = 0; i < (n); i++)
 
+string convert(string n) {
+    // 8進数から10進数へ変換
+    ll a = 0;
+    ll d = 1;
+    for (ll i = n.size() - 1; 0 <= i; i--) {
+        a += d * (n[i] - '0');
+        d *= 8;
+    }
+
+    // 10進数から9進数へ変換
+    string b = "";
+    while (0 < a) {
+        string s = to_string(a % 9);
+        if (s == "8") s = "5";
+        b = s + b;
+        a /= 9;
+    }
+    return b;
+}
+
 int main() {
+    string n;
+    ll k;
+    cin >> n >> k;
+    if (n == "0") {
+        cout << 0 << endl;
+        return 0;
+    }
+    rep(i, k) {
+        n = convert(n);
+    }
+    cout << n << endl;
     return 0;
 }
